8c332e2c00792175054274dfa3bdf44328d74088
Ehsan Ahmed <ehsanahmed122001@gmail.com>
2024-10-06 00:10:25 +0500

    config: add libs + img domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: [
+      "res.cloudinary.com",
+      "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+    ],
   },
 };
 
