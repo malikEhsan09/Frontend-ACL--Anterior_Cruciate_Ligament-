95adc3d5264dca3f49e38494f0c8d4a0d4f1bd89
Ehsan Ahmed <ehsanahmed8182@gmail.com>
2024-10-15 21:56:50 +0500

    adjust the sidebar from js to tsx

---
+++ src/app/(dashboard)/player/(pages)/payment/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Payment = () => {
+  return (
+    <div>
+      This is the payment module after sucessfully transaction payment will be
+      shown on this dashboard
+    </div>
+  );
+};
+
+export default Payment;
