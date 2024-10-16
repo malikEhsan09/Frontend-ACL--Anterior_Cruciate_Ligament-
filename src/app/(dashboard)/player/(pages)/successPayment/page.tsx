a7559ca59cc529f9c3c94b158f0191f3e6aa8327
Ehsan Ahmed <ehsanahmed8182@gmail.com>
2024-10-16 20:28:37 +0500

    add stripe key in env + add stripe payment pages

---
+++ src/app/(dashboard)/player/(pages)/successPayment/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PaymentSuccess = () => {
+  return <div className="text-green-600">Your payment has been successfully trasnacted</div>;
+};
+
+export default PaymentSuccess;
---
+++ src/app/(dashboard)/player/complete/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CompleteCheckout = () => {
+  return (
+    <div>
+      <h1 className="text-center font-bold text-xl text-black">
+        Payment Successfull{" "}
+      </h1>
+    </div>
+  );
+};
+
+export default CompleteCheckout;
