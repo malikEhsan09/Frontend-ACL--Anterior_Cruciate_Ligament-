bd776bda63a31a15804c2f0ff289718b22fc59bb
Ehsan Ahmed <ehsanahmed8182@gmail.com>
2024-11-01 20:00:16 +0500

    delete upload MRI folder from admin component

--- src/app/(dashboard)/admin/(pages)/uploadMri/not-found.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NotFound = () => {
-  return <div>MRI not found</div>;
-};
-
-export default NotFound;
--- src/app/(dashboard)/admin/(pages)/uploadMri/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const UploadMRIAdmin = () => {
-  return <div> upload MRI admin</div>;
-};
-
-export default UploadMRIAdmin;
